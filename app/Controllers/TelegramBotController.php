7698921b0641c36a872e37b53cbb50d42de1c63c
unknown <dsvan88@gmail.com>
2022-07-20 00:26:00 +0300

    v0.01b Update and test set command

--- app/Controllers/TelegramBotController.php
+++ app/Controllers/TelegramBotController.php
@@ -551,7 +551,7 @@ class TelegramBotController extends Controller
         if (!$result) {
             return ['result' => false, 'message' => json_encode($weekData['data'][$dayNum], JSON_UNESCAPED_UNICODE)];
         }
-        return ['result' => true, 'message' => $method === '-' ? '{{ Tg_Command_Successfully_Canceled }}' : Days::getFullDescription($weekData, $dayNum) . ($tournament ? ' Турнир ' : ' Не турнир ' . $index . ' ' . json_encode($weekData['data'][$dayNum]['mods']))];
+        return ['result' => true, 'message' => $method === '-' ? '{{ Tg_Command_Successfully_Canceled }}' : Days::getFullDescription($weekData, $dayNum)];
     }
     public static function recallCommand($data)
     {
