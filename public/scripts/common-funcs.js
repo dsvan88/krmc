a99be00fcb0e74489c924ce1e373c48325c6a456
unknown <dsvan88@gmail.com>
2022-11-25 16:44:55 +0200

    Fix small bug

--- public/scripts/common-funcs.js
+++ public/scripts/common-funcs.js
@@ -291,6 +291,7 @@ async function postAjax({ url, data,formData, successFunc, errorFunc, method = '
 		// });
 		if (response.ok) {
 			let description = response.headers.get['content-description'];
+			console.log(description);
 			if (description && description  === "File Transfer"); {
 				let filename = response.headers.get("content-disposition").replace(/^.*?=/, '').slice(1,-1);
 				let blob = await response.blob();
