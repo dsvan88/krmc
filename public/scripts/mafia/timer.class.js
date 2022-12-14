454a94a4dd95ba0de1121ed179d954e9c29d39ea
unknown <dsvan88@gmail.com>
2022-12-14 20:05:49 +0200

    v0.11 Increase  timer buttons

--- public/scripts/mafia/timer.class.js
+++ public/scripts/mafia/timer.class.js
@@ -46,12 +46,14 @@ class GameTimer {
 		if (!this.audioContext) {
 			this.audioContext = new AudioContext();
 		}
-		if (!this.MainTimer){
-			this.MainTimer = setInterval(() => this.countdown(), this.timerStep);
-		}
+
+		if (this.MainTimer) return false;
+		
+		this.MainTimer = setInterval(() => this.countdown(), this.timerStep);
 	}
 	pause() {
 		clearInterval(this.MainTimer);
+		this.MainTimer = null;
 	}
 	reset() {
 		this.pause();
