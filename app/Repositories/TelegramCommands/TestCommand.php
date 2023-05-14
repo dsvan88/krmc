73ace1a1e1d76992822577dea41575884ae90e55
unknown <dsvan88@gmail.com>
2023-05-14 20:18:32 +0300

    v0.11b Minor bugfix: Test command for Telegramm

--- app/Repositories/TelegramCommands/TestCommand.php
+++ app/Repositories/TelegramCommands/TestCommand.php
@@ -3,7 +3,7 @@ namespace app\Repositories\TelegramCommands;
 
 use app\core\ChatCommand;
 
-class WeekCommand extends ChatCommand {
+class TestCommand extends ChatCommand {
     public static function description(){
         return self::locale('<u>/day (week day)</u> <i>// Booking information for a specific day. Without specifying the day - for today</i>');
     }
