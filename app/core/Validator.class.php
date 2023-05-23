99c020373fc503c5127a6433c8c556a208ce4d24
dsvan88 <dsvan88@gmail.com>
2023-05-23 09:18:09 +0300

    v0.129b Minor bugfix: Validator class change retun values types in mixed

--- app/core/Validator.class.php
+++ app/core/Validator.class.php
@@ -1,17 +1,21 @@
 <?
+
 namespace app\core;
 
 class Validator
 {
-    public static function validate(string $method, mixed $value): string | bool{
+    public static function validate(string $method, mixed $value): mixed
+    {
         return self::$method($value);
     }
-    private static function email(string $value): string | bool {
+    private static function email(string $value): mixed
+    {
         $value = trim($value);
         if (empty($value)) return false;
         return filter_var($value, FILTER_VALIDATE_EMAIL) ? $value : false;
     }
-    private static function telegram(string $value): string | bool {
+    private static function telegram(string $value): mixed
+    {
         $value = trim($value);
         if (strlen($value) < 5)
             return false;
@@ -19,16 +23,18 @@ class Validator
             return false;
         return $value;
     }
-    private static function phone(string $value): string | bool {
+    private static function phone(string $value): mixed
+    {
         $value = trim($value);
         if (empty($value)) return false;
         $_clearPhone = preg_replace('/[^+0-9]/i', '', $value);
         $pattern = "/^(\+38){0,1}\d{10}$/";
         return preg_match($pattern, $_clearPhone) === 1 ? $value : false;
     }
-    private static function gender(string $value): string | bool {
+    private static function gender(string $value): string
+    {
         $value = trim($value);
         if (empty($value)) return '';
-        return in_array($value, ['male', 'female', 'secret'], true) ? $value : false;
+        return in_array($value, ['male', 'female', 'secret'], true) ? $value : '';
     }
 }
