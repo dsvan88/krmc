3ca1a561ec520c86c6f6b02c3bbce4ea1f3fc32c
unknown <dsvan88@gmail.com>
2023-10-16 11:18:44 +0300

    v0.163b Update: test openGraph for telegram day command

--- app/core/TelegramBot.class.php
+++ app/core/TelegramBot.class.php
@@ -36,7 +36,7 @@ class TelegramBot
         if ($message !== '') {
             $params['text'] = $message;
             $params['parse_mode'] = 'HTML';
-            // $params['disable_web_page_preview'] = true;
+            $params['disable_web_page_preview'] = true;
         }
         if ($messageId !== -1) {
             $params['reply_to_message_id'] = $messageId;
