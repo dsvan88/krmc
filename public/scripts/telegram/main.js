bd8d189d3bda8f93493c5274b8ed4d8121ea6e31
dsvan88 <dsvan88@gmail.com>
2024-06-28 11:20:39 +0300

    Minor update

--- public/scripts/telegram/main.js
+++ public/scripts/telegram/main.js
@@ -2,11 +2,15 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 
 const tgDataField = document.querySelector('#userdata');
+let tgData = tg.initDataUnsafe();
 try {
-
-    const tgData = tg.initDataUnsafe();
     tgDataField.innerHTML = JSON.stringify(tgData);
 } catch (throwed) {
     tgDataField.innerText = JSON.stringify(throwed);
 }
-tgDataField.innerText += 'Done!';
+tgDataField.innerHTML += '<br>Unsafe is done!';
+
+tgData = tg.initData();
+tgDataField.innerHTML += `<br>${tgData}`;
+
+tgDataField.innerHTML += '<br>Safe is done!';
