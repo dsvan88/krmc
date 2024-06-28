066b70872cffe1583124ceebde725dc8973ac8c3
dsvan88 <dsvan88@gmail.com>
2024-06-28 14:49:34 +0300

    Set langing page as auth with planing to redirect to user and admin views

--- public/scripts/telegram/auth.js
+++ public/scripts/telegram/auth.js
@@ -19,3 +19,33 @@ async function telegramAuth(string) {
 actionHandler.noticer.add({ message: 'Loaded.' });
 
 telegramAuth(window.Telegram.WebApp.initData);
+
+
+const tgDataField = document.querySelector('#userdata');
+try {
+
+    let tg = window.Telegram.WebApp;
+    tg.expand();
+
+    let tgData = tg.initDataUnsafe;
+
+    tgDataField.innerHTML = JSON.stringify(tgData);
+
+    tgDataField.innerHTML += '<br>Unsafe is done!';
+
+    tgData = tg.initData;
+    tgDataField.innerHTML += `<br>${tgData}`;
+
+    tgDataField.innerHTML += '<br>Safe is done!';
+
+} catch (error) {
+    tgDataField.innerHTML += `
+    <div>
+        <h3>Error:</h3>
+        <p>
+            <span>${error.name}:</span>
+            <p>${error.message}</p>
+            <p>${error.stack}</p>
+        </p>
+    </div>`;
+}
