86cd022616eaf3512c06575d712ad6630d3334ad
dsvan88 <dsvan88@gmail.com>
2024-06-28 10:38:18 +0300

    Start to development of Telegram App

--- public/scripts/telegram/main.js
+++ public/scripts/telegram/main.js
@@ -1,4 +1,11 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 
-document.querySelector('#userdata').innerText = tg.initData;
+const tgDataField = document.querySelector('#userdata');
+const tgData = tg.initDataUnsafe();
+
+let html = '';
+for (const [key, value] of Object.entries(tgData)) {
+    string += `<div>${key}:</div><p>${value}</p>`;
+}
+tgDataField.innerHTML = html;
