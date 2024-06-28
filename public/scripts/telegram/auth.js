05d58abb401abba43f6336fa9ccd95d47d90ce69
dsvan88 <dsvan88@gmail.com>
2024-06-28 14:39:07 +0300

    Set langing page as auth with planing to redirect to user and admin views

--- public/scripts/telegram/auth.js
+++ public/scripts/telegram/auth.js
@@ -1,13 +1,17 @@
 actionHandler.telegramAuth = async function () {
-    const formData = new FormData();
-    formData.append('data', window.Telegram.WebApp.initData)
-    result = await request({
-        url: 'verification/telegram/hmac',
-        data: formData,
-    });
+    try {
+        const formData = new FormData();
+        formData.append('data', window.Telegram.WebApp.initData)
+        result = await request({
+            url: 'verification/telegram/hmac',
+            data: formData,
+        });
 
-    if (!result['result'])
-        return this.noticer.add({ type: 'error', message: 'Auth error!' });
+        if (!result['result'])
+            return this.noticer.add({ type: 'error', message: 'Auth error!' });
 
-    this.noticer.add({ message: 'Auth complete!' });
+        this.noticer.add({ message: 'Auth complete!' });
+    } catch (error) {
+        this.noticer.add({ type: 'error', message: error.message });
+    }
 }
