d1467de52d1bb97b4fd3b3c22b84c4a6fe659c07
dsvan88 <dsvan88@gmail.com>
2024-07-09 12:26:03 +0300

    v0.17 Bugfix in rename function

--- app/Repositories/DayRepository.php
+++ app/Repositories/DayRepository.php
@@ -10,21 +10,20 @@ class DayRepository
     public static function renamePlayer(int $userId, string $name): void
     {
         $weeks = Weeks::getAll();
-        foreach ($weeks as $num => $week) {
-            if (!strpos($week['data'], '"id":' . $userId . ',')) continue;
-            $days = json_decode($week['data'], true);
-            foreach ($days as $dayNum => $day) {
+        foreach ($weeks as $week) {
+            foreach ($week['data'] as $dayNum => $day) {
                 foreach ($day['participants'] as $participantNum => $participant) {
                     if ($participant['id'] !== $userId) continue;
-                    $days[$dayNum]['participants'][$participantNum]['name'] = $name;
+                    $week['data'][$dayNum]['participants'][$participantNum]['name'] = $name;
                 }
             }
-            Weeks::setWeekData($week['id'], ['data' => $days]);
+            Weeks::setWeekData($week['id'], ['data' => $week['data']]);
         }
     }
-    public static function dayDescription(array $day):string{
+    public static function dayDescription(array $day): string
+    {
         if (empty($day)) return false;
-        $result = $day['date'] . ' - ' .  $day['gameName']."\n".Locale::phrase('Already registered players').': '.count($day['participants']).PHP_EOL;
+        $result = $day['date'] . ' - ' .  $day['gameName'] . "\n" . Locale::phrase('Already registered players') . ': ' . count($day['participants']) . PHP_EOL;
         return preg_replace('/<.*?>/', '', $result);
     }
 }
