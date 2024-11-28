3a4da8a02717138cc79669c3f925a3cb0119af11
dsvan88 <dsvan88@gmail.com>
2024-11-28 20:17:03 +0200

    v0.171 minor test header menu for telegram app

--- app/views/layouts/empty.php
+++ app/views/layouts/empty.php
@@ -2,29 +2,39 @@
 <html lang="en">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <? #<meta name="description" content="<?= $webDescription ? >" />
-    ?>
-    <meta name="author" content="<?= CFG_AUTHOR ?>" />
-    <link rel="stylesheet" href="/public/css/style.css?v='<?= $_SERVER['REQUEST_TIME'] ?>" />
-    <?= $scripts ?>
-    <?= $styles ?>
-    <title><?= $title . ' ' . CLUB_SNAME . ' v' . APP_VERSION ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <? if (!empty($description)) : ?>
+        <meta name="description" content="<?= $description ?>">
+    <? endif ?>
+    <meta name="author" content="<?= CFG_AUTHOR ?>">
+    <link rel="stylesheet" href="/public/css/style.css?v=<?= $_SERVER['REQUEST_TIME'] ?>">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro">
+
+    <script defer="" src="/public/scripts/request.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
+    <script defer="" src="/public/scripts/action-handler.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
+    <script defer="" src="/public/scripts/popups.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
+    <script defer="" src="/public/scripts/noticer.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
+    <script defer="" src="/public/scripts/common-funcs.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
+    <? if (!empty($scripts)) : ?>
+        <? if (is_string($scripts)) : ?>
+            <script defer="" src="<?= SCRIPTS_STORAGE . $scripts . '?v=' . $_SERVER['REQUEST_TIME'] ?>"></script>
+            <? else :
+            for ($x = 0; $x < count($scripts); $x++) : ?>
+                <script defer="" src="<?= SCRIPTS_STORAGE . $scripts[$x] . '?v=' . $_SERVER['REQUEST_TIME'] ?>"></script>
+    <? endfor;
+        endif;
+    endif; ?>
+    <script defer="" src="/public/scripts/common.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
+    <script defer="" src="/public/scripts/modals.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
+    <link rel="icon" type="image/x-icon" href="/public/images/mafia-vintage-logo-short.svg">
+    <?= $locales ?>
+    <title><?= (isset($pageTitle) ? $pageTitle  : $title) . ' | ' . CLUB_SNAME . ' v' . APP_VERSION ?></title>
+    <? if (!empty($og)) self::component('open-graph', ['og' => $og]) ?>
 </head>
 
 <body>
     <div class="wrapper">
-        <header class="header">
-            <?= $header ?>
-        </header>
-        <div class="header-for-auto-scroll" id="start-page"></div>
-        <main class="main">
-            <?= $content ?>
-        </main>
-        <footer class="footer">
-            <div class="footer_content"><?= $footerContent ?></div>
-        </footer>
     </div>
 </body>
 
