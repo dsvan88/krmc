4ef565f029ab15de16fb4a7bfdf8ebfc28ab9dd4
dsvan88 <dsvan88@gmail.com>
2025-01-28 17:45:46 +0200

    v0.18.1b SOmething went wrong. Check

--- app/Controllers/TelegramBotController.php
+++ app/Controllers/TelegramBotController.php
@@ -51,8 +51,6 @@ class TelegramBotController extends Controller
         $data = trim(file_get_contents('php://input'));
         $message = json_decode($data, true);
 
-        Sender::message(900669168, $data);
-
         if (!is_array($message) || empty($message['message']) || empty($message['message']['text'])) {
             die('{"error":"1","title":"Error!","text":"Error: Nothing to get."}');
         }
@@ -98,6 +96,7 @@ class TelegramBotController extends Controller
             return false;
         }
 
+        Sender::message(900669168, $data);
         if (!$command) return false;
     }
     public static function webhookAction()
