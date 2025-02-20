1e2eed1d1bbbc4ed63c5a0140650432247ea77ac
dsvan88 <dsvan88@gmail.com>
2025-02-20 20:00:08 +0200

    v0.19b Minor update: add base for add new image to list

--- public/scripts/images.js
+++ public/scripts/images.js
@@ -1,7 +1,7 @@
 
 actionHandler.newImage = function (event) {
     console.log(event);
-    // let file = event.target.files[0];
+    let file = event.target.files[0];
     // let img = createNewElement({
     //     tag: 'img',
     //     style: 'height:40vh;width:auto',
