9a7f05a06d9d08b593bb23966230086110372342
dsvan88 <dsvan88@gmail.com>
2025-09-05 09:26:12 +0300

    v0.25b Test bugfix: Telegram Web App empty test layout with concated scripts

--- app/views/layouts/empty.php
+++ app/views/layouts/empty.php
@@ -21,22 +21,27 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro">
 
     <script defer="" src="https://telegram.org/js/telegram-web-app.js"></script>
+    <?
+    /*
     <script defer="" src="/public/scripts/request.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
     <script defer="" src="/public/scripts/action-handler.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
     <script defer="" src="/public/scripts/popups.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
     <script defer="" src="/public/scripts/noticer.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
     <script defer="" src="/public/scripts/common-funcs.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
-    <? if (!empty($scripts)) : ?>
-        <? if (is_string($scripts)) : ?>
-            <script defer="" src="<?= static::$scriptsPath . '/' . $scripts . '?v=' . $_SERVER['REQUEST_TIME'] ?>"></script>
-            <? else :
-            for ($x = 0; $x < count($scripts); $x++) : ?>
-                <script defer="" src="<?= static::$scriptsPath . '/' . $scripts[$x] . '?v=' . $_SERVER['REQUEST_TIME'] ?>"></script>
-    <? endfor;
-        endif;
-    endif; ?>
     <script defer="" src="/public/scripts/common.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
     <script defer="" src="/public/scripts/modals.js?v=<?= $_SERVER['REQUEST_TIME'] ?>"></script>
+    <? if (!empty($scripts)) : ?>
+        <? if (is_string($scripts)) : ?>
+            <script defer="" src="<?= SCRIPTS_STORAGE . $scripts . '?v=' . $_SERVER['REQUEST_TIME'] ?>"></script>
+            <? else :
+            for ($x = 0; $x < count($scripts); $x++) : ?>
+                <script defer="" src="<?= SCRIPTS_STORAGE . $scripts[$x] . '?v=' . $_SERVER['REQUEST_TIME'] ?>"></script>
+            <? endfor ?>
+        <? endif ?>
+    <? endif ?>
+    */
+    ?>
+    <script defer="" src="<?= static::$scriptsPath . $scripts . (self::$refresh ? '' : '?v=' . $_SERVER['REQUEST_TIME']) ?>"></script>
     <link rel="icon" type="image/x-icon" href="/public/images/mafia-vintage-logo-short.svg">
     <?= $locales ?>
     <title><?= (isset($pageTitle) ? $pageTitle  : $title) . ' | ' . CLUB_SNAME . ' v' . APP_VERSION ?></title>
