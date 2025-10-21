e3d37f9b66c272d4418e749ede39fd2412e9ffca
dsvan88 <dsvan88@gmail.com>
2025-10-21 15:09:21 +0300

    v0.294b Improved booking show view

---
+++ public/scripts/d4a11f776760b553.js
@@ -0,0 +1,19 @@
+actionHandler.accountRegisterFormSubmit = async function (event, formData, modal) {
+    const self = this;
+    event.preventDefault();
+    const verification = await self.verification(event.target, 'verification/register/name');
+
+    if (!formData) formData = new FormData(event.target);
+
+    if (verification) {
+        formData.append('code', verification);
+    }
+    await request({
+        url: 'account/register',
+        data: formData,
+        success: (result) => {
+            self.commonResponse.call(self, result);
+            modal.unpause();
+        },
+    });
+}
