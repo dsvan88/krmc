2ff480d5e820e82c5adcafcc95e7c05f77217144
dsvan88 <dsvan88@gmail.com>
2025-11-07 16:28:54 +0200

    v0.298.5b minor bugfix: improve usability of infotiles

--- app/scripts/images-pad.js
+++ app/scripts/images-pad.js
@@ -117,13 +117,13 @@ class CustomImagesPad extends Prompt {
     }
     mouseEnterEvent(e) {
         this.mouseOver = e.target;
+        this.infoTile.style.left = (e.clientX + 20) + 'px';
+        this.infoTile.style.top = (e.clientY + 20) + 'px';
         this.infoTile.classList.remove('hidden');
         const index = this.mouseOver.index;
         this.infoTile.rows[0].innerText = this.images[index].name;
         this.infoTile.rows[1].innerText = this.images[index].size + ' Кб';
         this.infoTile.rows[2].innerText = this.images[index].resol;
-        this.infoTile.style.left = (e.clientX + 20) + 'px';
-        this.infoTile.style.top = (e.clientY + 20) + 'px';
     }
     mouseLeaveEvent() {
         this.mouseOver = null;
