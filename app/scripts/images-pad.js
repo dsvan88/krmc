d490fa9ea0a1ccf35f0b9107677227200b689226
dsvan88 <dsvan88@gmail.com>
2025-11-07 16:05:14 +0200

    v0.298.5b minor update: add info tile for images in images pad

--- app/scripts/images-pad.js
+++ app/scripts/images-pad.js
@@ -1,8 +1,10 @@
 class CustomImagesPad extends Prompt {
-    checkboxs = [];
-    values = [];
+    checkboxes = [];
+    images = [];
     addNewInput = null;
     nextPageButton = null;
+    mouseOver = null;
+    infoTile = null;
 
     constructor({
         title = "Images list",
@@ -39,23 +41,26 @@ class CustomImagesPad extends Prompt {
         this.showImagesPad();
 
         inputWrapper.append(imagesPad);
+        this.dialog.after(this.createInfoTile());
         return this;
     }
     showImagesPad() {
-        const y = this.checkboxs.length;
+        const y = this.checkboxes.length;
         for (let x = y; x < this.images.length; x++) {
-            const checkboxWrapper = document.createElement('span');
+            const checkboxWrapper = document.createElement('label');
             checkboxWrapper.classList.add('images__item');
-            this.checkboxs[x] = document.createElement('input');
-            this.checkboxs[x].type = 'checkbox';
-            this.checkboxs[x].id = `checkbox[${x}]`;
-            this.checkboxs[x].value = x;;
+            this.checkboxes[x] = document.createElement('input');
+            this.checkboxes[x].type = 'checkbox';
+            this.checkboxes[x].id = `checkbox[${x}]`;
+            this.checkboxes[x].value = x;
+            checkboxWrapper.htmlFor = this.checkboxes[x].id;
 
             const img = document.createElement('img');
             img.src = this.images[x].thumbnailLink;
             img.classList.add('images__image');
-            checkboxWrapper.append(this.checkboxs[x]);
+            checkboxWrapper.append(this.checkboxes[x]);
             checkboxWrapper.append(img);
+            this.images[x].node = checkboxWrapper;
             this.nextPageButton.before(checkboxWrapper);
         }
     }
@@ -63,6 +68,11 @@ class CustomImagesPad extends Prompt {
         const s = this;
         this.addNewInput.addEventListener('change', (e) => s.addNewImage.call(s, e));
         this.nextPageButton.addEventListener('click', (e) => s.getMoreImages.call(s, e));
+        for (const image of this.images) {
+            image.node.addEventListener('mouseenter', (e) => s.mouseEnterEvent.call(s, e));
+            image.node.addEventListener('mouseleave', (e) => s.mouseLeaveEvent.call(s));
+            image.node.addEventListener('mousemove', (e) => s.mouseMoveEvent.call(s, e));
+        }
     }
     getNewImageForm() {
         const newImageForm = document.createElement('form');
@@ -91,6 +101,35 @@ class CustomImagesPad extends Prompt {
 
         return this.nextPageButton;
     }
+    createInfoTile() {
+        this.infoTile = document.createElement('div');
+        this.infoTile.classList.add('image__info', 'info', 'hidden');
+        this.infoTile.rows = [];
+
+        for (let x = 0; x < 3; x++) {
+            const row = document.createElement('div');
+            row.classList.add('info__row');
+            this.infoTile.append(row);
+            this.infoTile.rows.push(row);
+        }
+        return this.infoTile;
+    }
+    mouseEnterEvent(e) {
+        this.mouseOver = e.target;
+        this.infoTile.classList.remove('hidden');
+        this.infoTile.rows[0].innerText = this.images[this.mouseOver.htmlFor.slice(-2, -1)].name;
+        this.infoTile.rows[1].innerText = this.images[this.mouseOver.htmlFor.slice(-2, -1)].size + ' Кб';
+        this.infoTile.rows[2].innerText = this.images[this.mouseOver.htmlFor.slice(-2, -1)].resol;
+    }
+    mouseLeaveEvent() {
+        this.mouseOver = null;
+        this.infoTile.classList.add('hidden');
+    }
+    mouseMoveEvent(e) {
+        if (!this.mouseOver) return false;
+        this.infoTile.style.left = (e.clientX + 20) + 'px';
+        this.infoTile.style.top = (e.clientY + 20) + 'px';
+    }
     async addNewImage(event) {
         const self = this;
         const file = event.target.files[0];
@@ -99,7 +138,7 @@ class CustomImagesPad extends Prompt {
         checkboxWrapper.classList.add('images__item');
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
-        checkbox.id = `checkbox[${this.checkboxs.length}]`;
+        checkbox.id = `checkbox[${this.checkboxes.length}]`;
         checkbox.value = URL.createObjectURL(file);
         checkbox.disabled = true;
 
@@ -109,7 +148,7 @@ class CustomImagesPad extends Prompt {
 
         checkboxWrapper.append(checkbox);
         checkboxWrapper.append(img);
-        this.checkboxs.push(checkbox);
+        this.checkboxes.push(checkbox);
 
         this.addNewInput.closest('.images__form').after(checkboxWrapper);
         checkbox.addEventListener('change', () => self.updateInput.call(self, checkbox))
@@ -148,7 +187,7 @@ class CustomImagesPad extends Prompt {
     }
     submit() {
         const result = [];
-        for (const checkbox of this.checkboxs) {
+        for (const checkbox of this.checkboxes) {
             if (!checkbox.checked) continue;
             result.push(this.images[checkbox.value]);
         }
