0d20841f7e94bbad00bc0e6985776c3e5acb7eeb
dsvan88 <dsvan88@gmail.com>
2025-12-12 15:34:36 +0200

    minor progress in the tg_bot_repository, add cfg_maintance mode for tg bot

--- app/Controllers/TelegramBotController.php
+++ app/Controllers/TelegramBotController.php
@@ -91,6 +91,7 @@ class TelegramBotController extends Controller
 
         $userTelegramId = self::$incomeMessage[static::$type]['from']['id'];
         $userId = Contacts::getUserIdByContact('telegramid', $userTelegramId);
+
         if (static::$type === 'message') {
             $text = trim(self::$incomeMessage['message']['text']);
             $command = self::parseCommand($text);
