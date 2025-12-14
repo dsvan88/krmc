8c8a4ab5292b19b68d89473b6728e342da66a126
unknown <dsvan88@gmail.com>
2025-12-14 12:07:31 +0200

    Let's test!

--- app/Repositories/TelegramBotRepository.php
+++ app/Repositories/TelegramBotRepository.php
@@ -283,8 +283,8 @@ class TelegramBotRepository
     {
         foreach ($data as $i => $row) {
             foreach ($row as $k => $v) {
-                $data[$i]['text'] = Locale::phrase($row['text']);
-                $data[$i]['callback_data'] = static::replyButtonEncode($row['callback_data']);
+                $data[$i][$k]['text'] = Locale::phrase($v['text']);
+                $data[$i][$k]['callback_data'] = static::replyButtonEncode($v['callback_data']);
             }
         }
     }
