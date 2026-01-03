df99d6e9fd93a69a92ce681d0fd7103aea0ebac3
unknown <dsvan88@gmail.com>
2026-01-03 12:21:43 +0200

    v0.368 Reworked chat commands and answers. Looks fine.

--- app/Repositories/TelegramCommands/BookingCommand.php
+++ app/Repositories/TelegramCommands/BookingCommand.php
@@ -114,9 +114,9 @@ class BookingCommand extends ChatCommand
         ];
 
         if (!TelegramBotRepository::isDirect()){
-                if (count($weekData['data'][static::$arguments['dayNum']]['participants']) > 0) {
-                    $update['replyMarkup']['inline_keyboard'][0][] = ['text' => '❌' . self::locale('Opt-out'), 'callback_data' => ['c' => 'booking', 'w' => $weekId, 'd' => static::$arguments['dayNum'], 'r' => '1']];
-                }
+            if (count($weekData['data'][static::$arguments['dayNum']]['participants']) > 0) {
+                $replyMarkup['inline_keyboard'][0][] = ['text' => '❌' . self::locale('Opt-out'), 'callback_data' => ['c' => 'booking', 'w' => $weekId, 'd' => static::$arguments['dayNum'], 'r' => '1']];
+            }
         }
         elseif (in_array(self::$requester['id'], array_column($newDayData['participants'], 'id'))) {
             $replyMarkup['inline_keyboard'] = [
