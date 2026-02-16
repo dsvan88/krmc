9895cd6d1348f492dffaa6845bb8c953b72fb424
dsvan88 <dsvan88@gmail.com>
2026-02-16 12:20:46 +0200

    v0.383 Start to development of blocks method to construct website pages

---
+++ app/views/components/blocks/image.php
@@ -0,0 +1,6 @@
+<div class="block i__wrapper <?= $block['direction'] ?>">
+    <div class="i__image">
+        <img src="<?= $block['image'] ?> ?>" alt="" srcset="">
+        <div class="i__text"><?= $block['html'] ?></div>
+    </div>
+</div>
---
+++ app/views/components/blocks/text.php
@@ -0,0 +1,3 @@
+<div class="block t__wrapper <?= $block['direction'] ?>">
+    <div class="t__text"><?= $block['html'] ?></div>
+</div>
