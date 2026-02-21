77aff45c43b2b43e48efe2eeb3032e7773b785b4
unknown <dsvan88@gmail.com>
2026-02-22 01:12:32 +0200

    v0.383 Minor progress in the block method

--- app/views/components/blocks/forms/dashboard.php
+++ app/views/components/blocks/forms/dashboard.php
@@ -4,24 +4,24 @@ $selected = $selected ?? 'image';
 <ul class="block__dashboard dashboard">
     <li class="dashboard__item<?= $selected === 'text' ? ' selected' : '' ?>">
         <div class="dashboard__frame">
-            <p>T</p>
+            <p>TXT</p>
         </div>
     </li>
     <li class="dashboard__item<?= $selected === 'image' ? ' selected' : '' ?>">
         <div class="dashboard__frame image">
-            <p class="image">I</p>
+            <p class="image">IMG</p>
         </div>
     </li>
     <li class="dashboard__item<?= $selected === 'text-image' ? ' selected' : '' ?>">
         <div class="dashboard__frame  double">
-            <p>T</p>
-            <p class="image">I</p>
+            <p>TXT</p>
+            <p class="image">IMG</p>
         </div>
     </li>
     <li class="dashboard__item<?= $selected === 'image-text' ? ' selected' : '' ?>">
         <div class="dashboard__frame double">
-            <p class="image">I</p>
-            <p>T</p>
+            <p class="image">IMG</p>
+            <p>TXT</p>
         </div>
     </li>
 </ul>
--- app/views/components/blocks/forms/text.php
+++ app/views/components/blocks/forms/text.php
@@ -1,5 +1,5 @@
 <div class="block" data-block-type="text">
-    <?php self::component('blocks/forms/dashboard', ['selected' => 'image']) ?>
+    <?php self::component('blocks/forms/dashboard', ['selected' => 'text']) ?>
     <div class="editor-block" data-field="html[]">
         <div class="toolbar-container"></div>
         <div class="content-container">
