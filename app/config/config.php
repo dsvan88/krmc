7fd7758632346b70a787db6e7874fa0d90737793
unknown <dsvan88@gmail.com>
2026-05-10 17:49:56 +0300

    v0.45 Minor progress and bugfix

--- app/config/config.php
+++ app/config/config.php
@@ -14,6 +14,7 @@ if (!session_id()) {
     session_regenerate_id(true);
     if (empty($_SESSION['csrf'])){
         $_SESSION['csrf'] = bin2hex(random_bytes(32));
+        error_log('CSRF is regenerated.');
     }
 }
 
